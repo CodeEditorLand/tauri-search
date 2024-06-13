c19f4fe265858315965644d9d55aeb9ae4aa560c
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-13 18:30:41 +0300

    squash!

--- packages/tauri-search/src/cli/refresh-repos.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-import { refreshRepos } from "~/pipelines/refreshRepos";
-
-(async () => {
-  console.log(`- refresh Repo document cache`);
-  const { docs, cacheFile } = await refreshRepos();
-  console.log(`- all ${docs.length} repos have been updated and saved to: ${cacheFile}`);
-})();
